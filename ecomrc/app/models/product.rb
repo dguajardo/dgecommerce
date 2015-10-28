21c5737ee142521d31981e56c6bf81e86de0d6cc
Diego Guajardo <diego.guajardo@hotmail.cl>
2015-10-28 13:23:12 -0300

    adding has many relationships

--- ecomrc/app/models/product.rb
+++ ecomrc/app/models/product.rb
@@ -1,5 +1,10 @@
 class Product < ActiveRecord::Base
+	
 	has_many :reviews, dependent: :destroy
+	
+	has_many :likes, as: :likable
+	has_many :user_likes, through: :likes, source: :user
+	
 	belongs_to :user
 	belongs_to :category
 end
